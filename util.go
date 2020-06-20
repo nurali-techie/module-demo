a84d29a0dc7e3f3183858b43b947d5b5cb9dee40
nurali-techie <nurali.techie@gmail.com>
2020-06-20 11:52:53 +0530

    step4 - GenPw func added

--- util.go
+++ util.go
@@ -2,11 +2,17 @@ package util
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
+	"time"
 )
 
 func UserName(firstName, lastName string) string {
 	firstName = strings.ToLower(firstName)
 	lastName = strings.ToLower(lastName)
 	return fmt.Sprintf("%s-%s", firstName, lastName)
+}
+
+func GeneratePassword() string {
+	return strconv.Itoa(time.Now().Nanosecond())
 }
